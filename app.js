91ab66827323da33c6a8db07c4aa0bc3e2822fca
Sivanesh B <60127773+sivanesh-dsp@users.noreply.github.com>
2024-10-15 20:18:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Testing CI CD');
+  res.send('Testing CI/CD');
 });
 
 const PORT = process.env.PORT || 3000;
